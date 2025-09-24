3e200280d8c76f84b14564b3909cacac8d774bdc
[dyad] <randy@cvlcvc.com>
2025-09-24 08:53:24 -0400

    [dyad] Fixing HTML nesting and accessibility in navigation - wrote 1 file(s)

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import Link from 'next/link';
 import { IoMenu } from 'react-icons/io5';
 
 import { AccountMenu } from '@/components/account-menu';
 import { Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTrigger } from '@/components/ui/sheet';
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'; // Import SheetTitle
 import { getSession } from '@/features/account/controllers/get-session';
 
 import { signOut } from './(auth)/auth-actions';
@@ -28,12 +30,15 @@ export async function Navigation() {
             <SheetContent className='w-full bg-black'>
               <SheetHeader>
                 <Logo />
-                <SheetDescription className='py-8'>
-                  <Button variant='sexy' className='flex-shrink-0' asChild>
-                    <Link href='/signup'>Get started for free</Link>
-                  </Button>
-                </SheetDescription>
+                {/* Added SheetTitle for accessibility */}
+                <SheetTitle className="sr-only">Main Menu</SheetTitle> 
               </SheetHeader>
+              {/* Moved Button out of SheetDescription to fix HTML nesting error */}
+              <div className='py-8'>
+                <Button variant='sexy' className='flex-shrink-0' asChild>
+                  <Link href='/signup'>Get started for free</Link>
+                </Button>
+              </div>
             </SheetContent>
           </Sheet>
         </>
